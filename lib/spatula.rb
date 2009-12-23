2737fff2f59fe551e207b945417167b4a86b14ec
Trotter Cashion <cashion@gmail.com>
2009-12-22 20:47:57 -0500

    Add search command class

--- lib/spatula.rb
+++ lib/spatula.rb
@@ -3,62 +3,136 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-class Spatula
-  BASE_URL = "http://cookbooks.opscode.com/api/v1"
+module Spatula
+  class Spatula
+    BASE_URL = "http://cookbooks.opscode.com/api/v1"
+    @commands = {}
 
-  def self.run
-    command = ARGV.shift
-    Spatula.new.send(command, *ARGV)
-  end
-
-  # Search for cookbooks matching :query:
-  def search(query, start=0, items=10)
-    url = URI.parse("%s/search?q=%s&start=%s&items=%s" % [BASE_URL, query, start, items])
-    response = Net::HTTP.get(url)
-    items = JSON.parse(response)["items"]
-    items.each do |item|
-      puts [item["cookbook_name"], item["cookbook_description"], item["cookbook_maintainer"]].join("\t")
-    end
-  end
-
-  # Show the cookbook named :name:
-  def show(name)
-    print_response(get_cookbook_info(name))
-  end
-
-  # Show the latest version of the cookbook named :name:
-  def show_latest_version(name)
-    print_response(get_version_info(name))
-  end
-
-  # Install the cookbook :name: into cwd/cookbooks
-  #   Will create a cookbook_tarballs dir for storing downloaded tarballs
-  def install(name)
-    file = JSON.parse(get_version_info(name))["file"]
-    filename = File.basename(file)
-    FileUtils.mkdir_p("cookbook_tarballs")
-    `curl #{file} -o cookbook_tarballs/#{filename}`
-    `tar xzvf cookbook_tarballs/#{filename} -C cookbooks`
-  end
-
-  private
-    def get_cookbook_info(name)
-      url = URI.parse("%s/cookbooks/%s" % [BASE_URL, name])
-      Net::HTTP.get(url)
+    def self.register(command, klass)
+      @commands[command] = klass
     end
 
-    def get_version_info(name)
-      latest = JSON.parse(get_cookbook_info(name))["latest_version"]
-      response = Net::HTTP.get(URI.parse(latest))
-    end
-
-    def print_response(response)
-      item = JSON.parse(response)
-      item.each_pair do |k, v|
-        puts "#{k}:\t#{v}"
+    def self.run
+      command = ARGV.shift
+      if klass = @commands[command]
+        klass.run(*ARGV)
+      else
+        Spatula.new.send(command, *ARGV)
       end
     end
+
+    def search(query, start=0, items=10)
+    end
+
+    # Show the cookbook named :name:
+    def show(name)
+      print_response(get_cookbook_info(name))
+    end
+
+    # Show the latest version of the cookbook named :name:
+    def show_latest_version(name)
+      print_response(get_version_info(name))
+    end
+
+    # Install the cookbook :name: into cwd/cookbooks
+    #   Will create a cookbook_tarballs dir for storing downloaded tarballs
+    def install(name)
+      file = JSON.parse(get_version_info(name))["file"]
+      filename = File.basename(file)
+      FileUtils.mkdir_p("cookbook_tarballs")
+      `curl #{file} -o cookbook_tarballs/#{filename}`
+      `tar xzvf cookbook_tarballs/#{filename} -C cookbooks`
+    end
+
+    def prepare(server)
+    end
+
+    private
+      def strict_system *cmd
+        cmd.map! &:to_s
+        result = []
+
+        trace = cmd.join(' ')
+        warn trace if $TRACE
+
+        pid, inn, out, err = popen4(*cmd)
+
+        inn.sync   = true
+        streams    = [out, err]
+        out_stream = {
+          out => $stdout,
+          err => $stderr,
+        }
+
+        out_stream_buffers = {
+          out => "",
+          err => ""
+        }
+
+        # Handle process termination ourselves
+        status = nil
+        Thread.start do
+          status = Process.waitpid2(pid).last
+        end
+
+        until streams.empty? do
+          # don't busy loop
+          selected, = select streams, nil, nil, 0.1
+
+          next if selected.nil? or selected.empty?
+
+          selected.each do |stream|
+            if stream.eof? then
+              streams.delete stream if status # we've quit, so no more writing
+              next
+            end
+
+            data = stream.readpartial(1024)
+            out_stream[stream].write data
+            out_stream_buffers[stream] << data
+
+            if stream == err and out_stream_buffers[stream] =~ /password(?:for)?.*:/i then
+              out_stream_buffers[stream] = ""
+              inn.puts @password
+              data << "\n"
+              $stderr.write "\n"
+            end
+
+            result << data
+          end
+        end
+
+        unless status.success? then
+          raise "execution failed with status #{status.exitstatus}: #{cmd.join ' '}"
+        end
+
+        result.join
+      ensure
+        inn.close rescue nil
+        out.close rescue nil
+        err.close rescue nil
+      end
+
+      def get_cookbook_info(name)
+        url = URI.parse("%s/cookbooks/%s" % [BASE_URL, name])
+        Net::HTTP.get(url)
+      end
+
+      def get_version_info(name)
+        latest = JSON.parse(get_cookbook_info(name))["latest_version"]
+        response = Net::HTTP.get(URI.parse(latest))
+      end
+
+      def print_response(response)
+        item = JSON.parse(response)
+        item.each_pair do |k, v|
+          puts "#{k}:\t#{v}"
+        end
+      end
+  end
 end
+
+require 'spatula/search'
 
 if __FILE__ == $0
   Spatula.run
---
+++ lib/spatula/search.rb
@@ -0,0 +1,26 @@
+# Search for cookbooks matching :query:
+module Spatula
+  class Search
+    Spatula::Spatula.register("search", self)
+
+    def self.run(*args)
+      new(args).run
+    end
+
+    def initialize(query, start=0, count=10)
+      @query = query
+      @start = start
+      @count = count
+    end
+
+    def run
+      url = URI.parse("%s/search?q=%s&start=%s&items=%s" % [BASE_URL, @query, @start, @count])
+      response = Net::HTTP.get(url)
+      items = JSON.parse(response)["items"]
+      items.each do |item|
+        puts [item["cookbook_name"], item["cookbook_description"], item["cookbook_maintainer"]].join("\t")
+      end
+    end
+  end
+end
+
