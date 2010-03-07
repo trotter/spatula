6862fb04590d139031de7a838bec3d3e464e9d2a
Trotter Cashion <cashion@gmail.com>
2010-03-07 17:18:09 -0500

    Remove some unused code to make life better

--- lib/spatula.rb
+++ lib/spatula.rb
@@ -8,21 +8,6 @@ module Spatula
   BASE_URL = "http://cookbooks.opscode.com/api/v1"
 
   class Spatula < Thor
-#    @commands = {}
-#
-#    def self.register(command, klass)
-#      @commands[command] = klass
-#    end
-#
-#    def self.run
-#      command = ARGV.shift
-#      if klass = @commands[command]
-#        klass.run(*ARGV)
-#      else
-#        Spatula.new.send(command, *ARGV)
-#      end
-#    end
-
     desc "show COOKBOOK", "Show information about a cookbook"
     def show(name)
       print_response(get_cookbook_info(name))
@@ -62,71 +47,6 @@ module Spatula
     end
 
     private
-      def strict_system *cmd
-        cmd.map! &:to_s
-        result = []
-
-        trace = cmd.join(' ')
-        warn trace if $TRACE
-
-        pid, inn, out, err = popen4(*cmd)
-
-        inn.sync   = true
-        streams    = [out, err]
-        out_stream = {
-          out => $stdout,
-          err => $stderr,
-        }
-
-        out_stream_buffers = {
-          out => "",
-          err => ""
-        }
-
-        # Handle process termination ourselves
-        status = nil
-        Thread.start do
-          status = Process.waitpid2(pid).last
-        end
-
-        until streams.empty? do
-          # don't busy loop
-          selected, = select streams, nil, nil, 0.1
-
-          next if selected.nil? or selected.empty?
-
-          selected.each do |stream|
-            if stream.eof? then
-              streams.delete stream if status # we've quit, so no more writing
-              next
-            end
-
-            data = stream.readpartial(1024)
-            out_stream[stream].write data
-            out_stream_buffers[stream] << data
-
-            if stream == err and out_stream_buffers[stream] =~ /password(?:for)?.*:/i then
-              out_stream_buffers[stream] = ""
-              inn.puts @password
-              data << "\n"
-              $stderr.write "\n"
-            end
-
-            result << data
-          end
-        end
-
-        unless status.success? then
-          raise "execution failed with status #{status.exitstatus}: #{cmd.join ' '}"
-        end
-
-        result.join
-      ensure
-        inn.close rescue nil
-        out.close rescue nil
-        err.close rescue nil
-      end
-
       def get_cookbook_info(name)
         url = URI.parse("%s/cookbooks/%s" % [BASE_URL, name])
         Net::HTTP.get(url)
