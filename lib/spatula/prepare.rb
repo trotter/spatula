fc7cae4b7229aa7386a336925cbe04c63503aed0
Trotter Cashion <cashion@gmail.com>
2010-03-09 09:23:35 -0500

    Complain when they're not on ubuntu and we're preparing

--- lib/spatula/prepare.rb
+++ lib/spatula/prepare.rb
@@ -15,6 +15,19 @@ module Spatula
     end
 
     def run
+      send "run_for_#{os}"
+    end
+
+    def os
+      etc_issue = `cat /etc/issue`
+      if etc_issue =~ /ubuntu/i
+        "ubuntu"
+      else
+        raise "Sorry, we currently only support ubuntu for preparing. Please fork http://github.com/trotter/spatula and add support for your OS. I'm happy to incorporate pull requests."
+      end
+    end
+
+    def run_for_ubuntu
       ssh "sudo apt-get update"
       ssh "sudo aptitude -y install ruby irb ri libopenssl-ruby1.8 libshadow-ruby1.8 ruby1.8-dev gcc g++ rsync curl"
       ssh "curl -L 'http://rubyforge.org/frs/download.php/69365/rubygems-1.3.6.tgz' | tar xvzf -"
